b23a9dbb7860e05347f76d6813a75122ec7f72b3
Igor Bykov <igor.bykov@protonmail.com>
2020-08-11 01:14:31 +0200

    Jest adjustments

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
     name: "react-online-hook",
     setupFilesAfterEnv: ["jest-enzyme"],
-    testEnvironment: "enzyme",
-    transformIgnorePatterns: [
-        "<rootDir>/node_modules/(?!react-online-hook)"
-    ]
+    testEnvironment: "enzyme"
 }
--- tests/__mocks__/component.jsx
+++ tests/__mocks__/component.jsx
@@ -10,3 +10,5 @@ const BasicMockComponent = () => {
         </span>
     );
 };
+
+export default BasicMockComponent;
